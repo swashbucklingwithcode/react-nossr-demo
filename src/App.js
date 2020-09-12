37e81d7f7182961c1b2554f92195df0f53bb2692
Jimmy DC <jimmydalecleveland@gmail.com>
2020-09-11 18:32:05 -0600

    remove excess and add html

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>First Page</h1>
+      <p>You guessed it!</p>
+      <h1>First Page!</h1>
+      <h3>H3 after H1 GASP</h3>
+      <p>This is the first page</p>
+      <ul>
+        <li>list item one</li>
+        <li>here is #2</li>
+        <li>and a third</li>
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
